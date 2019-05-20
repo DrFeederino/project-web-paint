c7585b7e734a7ada293bffcdb61e26807db34307
Dr. Feederino <23013757+DrFeederino@users.noreply.github.com>
2019-05-21 00:49:21 +0900

    Fixed locales, added RU locale

--- client/src/locale/i18nClient.js
+++ client/src/locale/i18nClient.js
@@ -1,40 +1,35 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import {
-    ru,
-    en,
-} from './index';
+import { ru, en } from './index';
 
-const i18nClient = i18n
-    .use(LanguageDetector)
-    .init({
-        load: 'all',
-        whitelist: ['en', 'en-US', 'ru', 'ru-RU'],
-        nonExplicitWhitelist: false,
-        lngs: ['en-US', 'ru-RU'],
-        fallbackLng: 'en-US',
-        interpolation: {
-            escapeValue: false,
-        },
-        react: {
-            wait: true, // set to true if you like to wait for loaded in every translated hoc
-            nsMode: 'default', // set it to fallback to let passed namespaces to translated hoc act as fallbacks
-        },
-        defaultNS: 'locale.en',
-        resources: {
-            en: {
-                'locale.constant': en,
-            },
-            'en-US': {
-                'locale.constant': en,
-            },
-            ru: {
-                'locale.constant': ru,
-            },
-            'ru-RU': {
-                'locale.constant': ru,
-            },
-        },
-    });
+const i18nClient = i18n.use(LanguageDetector).init({
+  load: 'all',
+  whitelist: ['en', 'en-US', 'ru', 'ru-RU'],
+  nonExplicitWhitelist: false,
+  lngs: ['en-US', 'ru-RU'],
+  fallbackLng: 'en-US',
+  interpolation: {
+    escapeValue: false
+  },
+  react: {
+    wait: true, // set to true if you like to wait for loaded in every translated hoc
+    nsMode: 'default' // set it to fallback to let passed namespaces to translated hoc act as fallbacks
+  },
+  defaultNS: 'locale.en',
+  resources: {
+    en: {
+      'locale.en': en
+    },
+    'en-US': {
+      'locale.en': en
+    },
+    ru: {
+      'locale.ru': ru
+    },
+    'ru-RU': {
+      'locale.ru': ru
+    }
+  }
+});
 
 export default i18nClient;
